612db695558428c7c9128b6b939c0373dbdbf8aa
Olegas Sevcenko <olegas.sevcenko@elev.ga.ntig.se>
2022-03-28 10:46:43 +0200

    Table och session

--- School/labbar/17-registrering/admin.php
+++ School/labbar/17-registrering/admin.php
@@ -3,11 +3,12 @@ include('konfigdb.php');
 session_start();
 
 if (!isset($_SESSION['inloggad'])) {
-  $_SESSION['inloggad'] = false;
+    $_SESSION['inloggad'] = false;
 }
 ?>
 <!DOCTYPE html>
 <html lang="sv">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -16,6 +17,7 @@ if (!isset($_SESSION['inloggad'])) {
     <link rel="stylesheet" href="./style.css">
 
 </head>
+
 <body>
     <div class="container">
         <div>
@@ -23,9 +25,43 @@ if (!isset($_SESSION['inloggad'])) {
             include('./components/navbar.php')
             ?>
             <h1>Admin</h1>
+            <?php
+            // SQL satsen
+            $sql = "SELECT * FROM register";
+            // kör SQL Satsen
+            $resultat = $conn->query($sql);
+
+            if (!$resultat) {
+                die('<p class=\"alert alert-warning\">Gick inte att köra sql satsen</p>');
+            } else {
+                echo "<table class=\"table table-striped\">
+                <thead>
+                  <tr>
+                    <th scope=\"col\">#</th>
+                    <th scope=\"col\">Namn</th>
+                    <th scope=\"col\">Epost</th>
+                  </tr>
+                </thead>
+                <tbody>
+                ";
+                while ($rad = $resultat->fetch_assoc()) {
+                    echo "
+                    <tr>
+                    <th scope=\"row\">$rad[id]</th>
+                    <td>$rad[namn]</td>
+                    <td>$rad[epost]</td>
+                    </tr>
+                    ";
+                }
+                echo "</tbody>
+                </table>";
+            }
+
+            ?>
             <main>
             </main>
         </div>
     </div>
 </body>
+
 </html>
